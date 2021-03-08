b4a4c87c88109876bb8564ae02e51fdab7e4e704
IanKarlo <iankarlots@gmail.com>
2021-03-08 14:31:48 -0300

    Projeto Angular

---
+++ Angular/CRUD/frontend/src/app/components/product/product.service.ts
@@ -0,0 +1,54 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { EMPTY, Observable } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { Product } from './product.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  baseURL = 'http://localhost:3001/products'
+
+  constructor(private snackbar: MatSnackBar, private http: HttpClient) { }
+
+  showMessage(msg: string, isError: boolean = false):void {
+    this.snackbar.open(msg, 'X', {
+      duration: 3000, //3 seconds
+      horizontalPosition: "right",
+      verticalPosition: 'top',
+      panelClass: isError ? ['msg-error'] : ['msg-success'],
+    })
+  }
+
+  errorHandler(err: any): Observable<any> {
+    this.showMessage("Ocorreu um erro!", true);
+    return EMPTY;
+  }
+
+  create(product: Product): Observable<Product> {
+    return this.http.post<Product>(this.baseURL, product).pipe(map(obj => obj), catchError(e => this.errorHandler(e)));
+  }
+
+  read(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.baseURL).pipe(map(obj => obj), catchError(e => this.errorHandler(e)));
+  }
+
+  readById(id: string): Observable<Product> {
+    const url = `${this.baseURL}/${id}`;
+    return this.http.get<Product>(url).pipe(map(obj => obj), catchError(e => this.errorHandler(e)));
+  }
+
+  update(product: Product): Observable<Product> {
+    const url = `${this.baseURL}/${product.id}`;
+    return this.http.put<Product>(url, product).pipe(map(obj => obj), catchError(e => this.errorHandler(e)));
+  }
+
+  delete(id: string): Observable<Product> {
+    console.log("Roi");
+    const url = `${this.baseURL}/${id}`;
+    return this.http.delete<Product>(url).pipe(map(obj => obj), catchError(e => this.errorHandler(e)));
+  }
+}
---
+++ Angular/CRUD/frontend/src/app/components/template/header/header.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { HeaderData } from './header-data.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeaderService {
+
+  private _headerData = new BehaviorSubject<HeaderData>({
+    title: 'Início',
+    icon: 'home',
+    routeUrl: '',
+  });
+
+  public get headerData(): HeaderData {
+    return this._headerData.value;
+  }
+
+  public set headerData(headerData: HeaderData){
+    this._headerData.next(headerData);
+  }
+
+  constructor() { }
+}
